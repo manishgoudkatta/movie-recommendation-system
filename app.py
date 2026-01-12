def89de4d0e72f6a72b3bb581cee671fabc3749f
manishgoudkatta <manishgoud796@gmail.com>
2026-01-12 23:06:16 +0530

    Added movie_dict and changed app file

--- app.py
+++ app.py
@@ -1,38 +1,3 @@
-# import  streamlit as st
-# import pickle
-# import pandas as pd
-#
-#
-# def recommend(movie):
-#     movie_index = movies[movies['title'] == movie].index[0]
-#     distances = similarity[movie_index]
-#     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:13]
-#
-#     recommended_movies=[]
-#     for i in movies_list:
-#         recommended_movies.append(movies.iloc[i[0]].title)
-#     return recommended_movies
-#
-# st.header('Movie Recommender System')
-#
-# movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
-# movies = pd.DataFrame(movies_dict)
-#
-# similarity = pickle.load(open('similarity.pkl', 'rb'))
-#
-#
-# movie_list = movies['title'].values
-# selected_movie = st.selectbox(
-#     "How would you like to predict?",
-#     movies['title'].values
-# )
-#
-# if st.button('Recommend'):
-#     recommendations = recommend(selected_movie)
-#     for i in recommendations:
-#        st.write(i)
-
-
 import streamlit as st
 import pickle
 import pandas as pd
